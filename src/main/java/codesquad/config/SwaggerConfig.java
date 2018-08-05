ebcbd73178e7939264a568871e7ef1bdb78f2b44
우아테크캠프_6 <woowatechcamp_6@woowatechcamp6.local>
2018-08-05 13:09:55 +0900

    Spring profile 중복부분 제거
    1. properties 중복부분을 제거하였습니다.
       -환경에 따른 log파일을 설정하였습니다.

--- src/main/java/codesquad/config/SwaggerConfig.java
+++ src/main/java/codesquad/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@Profile({"dev","local"})
+@Profile({"dev"})
 public class SwaggerConfig {
 
     @Bean
